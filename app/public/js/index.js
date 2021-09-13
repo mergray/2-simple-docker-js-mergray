7ff99c185f3e3fd519efee5f58189738d1690b15
mergray <mergray@iu.edu>
2021-09-13 19:43:35 -0400

    Make Table Sortable

---
+++ app/public/js/index.js
@@ -0,0 +1,12 @@
+const Offer = {
+    data() {
+      return {
+        foo: 0,
+        msg:"D&S is my favorite class",
+        list: ["red","green","blue"]
+      }
+    }
+  }
+  
+  Vue.createApp(Offer).mount('#offerApp')
+
